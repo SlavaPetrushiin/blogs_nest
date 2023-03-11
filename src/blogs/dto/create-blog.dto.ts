575c9389c7e6f05beb4ae815312a2f873e736258
Slava <slava91petrushin@yandex.ru>
2023-03-11 15:34:53 +0300

    added Transform

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,17 +1,21 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
 
 export class CreateBlogDto {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(15)
   @IsNotEmpty()
   name: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(500)
   @IsNotEmpty()
   description: string;
 
   @IsUrl()
+  @Transform(({ value }) => value.trim())
   @IsNotEmpty()
   @MaxLength(100)
   websiteUrl: string;
