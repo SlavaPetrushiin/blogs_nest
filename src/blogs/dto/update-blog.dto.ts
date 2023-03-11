a068dc4315f80b3174c897dac8241625265f578b
Slava <slava91petrushin@yandex.ru>
2023-03-11 15:36:45 +0300

    fix dto

--- src/blogs/dto/update-blog.dto.ts
+++ src/blogs/dto/update-blog.dto.ts
@@ -1,17 +1,21 @@
+import { Transform } from 'class-transformer';
 import { IsString, IsUrl, MaxLength, IsNotEmpty } from 'class-validator';
 
 export class UpdateBlogDto {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(15)
   @IsNotEmpty()
   name: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(500)
   @IsNotEmpty()
   description: string;
 
   @IsUrl()
+  @Transform(({ value }) => value.trim())
   @MaxLength(100)
   @IsNotEmpty()
   websiteUrl: string;
