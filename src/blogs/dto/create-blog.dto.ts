6e9508559ab6812a73743d2a71ff73ede7d6fba1
Slava <slava91petrushin@yandex.ru>
2023-03-11 15:26:41 +0300

    added IsNotEmpty

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,15 +1,18 @@
-import { IsString, IsUrl, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
 
 export class CreateBlogDto {
   @IsString()
   @MaxLength(15)
+  @IsNotEmpty()
   name: string;
 
   @IsString()
   @MaxLength(500)
+  @IsNotEmpty()
   description: string;
 
   @IsUrl()
+  @IsNotEmpty()
   @MaxLength(100)
   websiteUrl: string;
 }
--- src/blogs/dto/update-blog.dto.ts
+++ src/blogs/dto/update-blog.dto.ts
@@ -1,15 +1,18 @@
-import { IsString, IsUrl, MaxLength } from 'class-validator';
+import { IsString, IsUrl, MaxLength, IsNotEmpty } from 'class-validator';
 
 export class UpdateBlogDto {
   @IsString()
   @MaxLength(15)
+  @IsNotEmpty()
   name: string;
 
   @IsString()
   @MaxLength(500)
+  @IsNotEmpty()
   description: string;
 
   @IsUrl()
   @MaxLength(100)
+  @IsNotEmpty()
   websiteUrl: string;
 }
