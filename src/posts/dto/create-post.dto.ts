ecfd6ce5d369b2f6d8a24e28d56c696462dbfe96
Slava <slava91petrushin@yandex.ru>
2023-03-11 16:36:04 +0300

    fix dto post

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,31 +1,39 @@
+import { Transform } from 'class-transformer';
 import { IsString, MaxLength } from 'class-validator';
 export class CreatePostDto {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(30)
   title: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(100)
   shortDescription: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(1000)
   content: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   blogId: string;
 }
 
 export class CreatePostByBlogIdDto {
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(30)
   title: string;
 
   @IsString()
+  @Transform(({ value }) => value.trim())
   @MaxLength(100)
   shortDescription: string;
 
-  @MaxLength(1000)
   @IsString()
+  @Transform(({ value }) => value.trim())
+  @MaxLength(1000)
   content: string;
 }
