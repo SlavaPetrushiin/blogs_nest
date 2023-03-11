5cf2a8b033b108aaebdc56089725d4595efebfa2
Slava <slava91petrushin@yandex.ru>
2023-03-11 14:53:53 +0300

    fix dto posts

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,20 +1,31 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 export class CreatePostDto {
   @IsString()
+  @MaxLength(100)
   title: string;
+
   @IsString()
+  @MaxLength(100)
   shortDescription: string;
+
   @IsString()
+  @MaxLength(100)
   content: string;
+
   @IsString()
   blogId: string;
 }
 
 export class CreatePostByBlogIdDto {
   @IsString()
+  @MaxLength(100)
   title: string;
+
   @IsString()
+  @MaxLength(100)
   shortDescription: string;
+
+  @MaxLength(100)
   @IsString()
   content: string;
 }
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,12 +1,18 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class UpdatePostDto {
   @IsString()
+  @MaxLength(30)
   title: string;
+
   @IsString()
+  @MaxLength(100)
   shortDescription: string;
+
   @IsString()
+  @MaxLength(100)
   content: string;
+
   @IsString()
   blogId: string;
 }
